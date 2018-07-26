fea56e5e126f3013f594e800d17261e78f0fb729
soeur <1034236490@qq.com>
2018-07-26 17:47:26 +0800

    update

--- src/main/java/net/systir/mcpe/nukkit/SoeurGrade/EventListener.java
+++ src/main/java/net/systir/mcpe/nukkit/SoeurGrade/EventListener.java
@@ -6,27 +6,34 @@ import cn.nukkit.event.Listener;
 import cn.nukkit.event.player.PlayerJoinEvent;
 import cn.nukkit.form.response.FormResponseModal;
 import cn.nukkit.form.window.FormWindowCustom;
+import cn.nukkit.scheduler.PluginTask;
 import net.systir.mcpe.nukkit.SoeurGrade.gui.ModalWindow;
 
 public class EventListener implements Listener {
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = false)
     public void onJoin(PlayerJoinEvent event) {
-        FormWindowCustom formWindowModal = new FormWindowCustom("1");
-        //System.out.println(formWindowModal.getJSONData());
-        event.getPlayer().showFormWindow(formWindowModal);
-        new ModalWindow()
-                .SetTitle("what?")
-                .SetContent("xxm is a sb?")
-                .SetTrueButtonText("yes")
-                .SetFalseButtonText("no")
-                .OnResponded((response, player) -> {
-                    player.sendMessage("you chose " + ((FormResponseModal) response).getClickedButtonText());
-                })
-                .OnClose((player) -> {
-                    player.sendMessage("天杀的, 你居然关闭了窗口");
-                })
-                .Send(event.getPlayer());
+        Main.This().getServer().getScheduler().scheduleDelayedTask(new PluginTask<Main>(Main.This()) {
+            @Override
+            public void onRun(int i) {
+                if (event.getPlayer().spawned) {
+                    new ModalWindow()
+                            .SetTitle("what?")
+                            .SetContent("xxm is a sb?")
+                            .SetTrueButtonText("yes")
+                            .SetFalseButtonText("no")
+                            .OnResponded((response, player) -> {
+                                player.sendMessage("you chose " + ((FormResponseModal) response).getClickedButtonText());
+                            })
+                            .OnClose((player) -> {
+                                player.sendMessage("天杀的, 你居然关闭了窗口");
+                            })
+                            .Send(event.getPlayer());
+                }else {
+                    Main.This().getServer().getScheduler().scheduleDelayedTask(this, 5);
+                }
+            }
+        }, 10);
     }
 
 }
